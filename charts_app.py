2348826747f18b5848a1bf65d73e36291060986b
nvlpjk <52489538+nvlpjk@users.noreply.github.com>
2024-10-05 17:30:52 +0900

    Update charts_app.py

--- charts_app.py
+++ charts_app.py
@@ -1,4 +1,5 @@
 import plotly.express as px
+import streamlit as st
 
 df = px.data.gapminder().query("year == 2007")
 fig = px.treemap(df, path=[px.Constant('World'), 'continent','country'], values = 'pop' , color = 'lifeExp')
