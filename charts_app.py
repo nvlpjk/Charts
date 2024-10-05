c45bf2555eebbb36e927d4987784467e30fac941
nvlpjk <52489538+nvlpjk@users.noreply.github.com>
2024-10-05 17:38:58 +0900

    Update charts_app.py

--- charts_app.py
+++ charts_app.py
@@ -6,3 +6,8 @@ st.title('Streamlit for charts')
 df1 = px.data.gapminder().query("year == 2007")
 fig1 = px.treemap(df1, path=[px.Constant('World'), 'continent','country'], values = 'pop' , color = 'lifeExp')
 st.plotly_chart(fig1)
+
+
+df = px.data.tips()
+fig = px.box(df, y = 'total_bill' , x = 'time' , points = 'all' , color = 'smoker')
+st.plotly_chart(fig)
