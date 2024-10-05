4c9d60401f003030845993ee5d8d0410dd7aab2e
nvlpjk <52489538+nvlpjk@users.noreply.github.com>
2024-10-05 17:34:55 +0900

    Update charts_app.py

--- charts_app.py
+++ charts_app.py
@@ -1,13 +1,8 @@
 import plotly.express as px
 import streamlit as st
 
-st.title('Streamlit for sin and cos fuctioni visualiztion')
-
-
-x_start = st.slider('x 시작값' ,  0.0, 10.0, 0.0)
-x_end = st.slider('x 시작값' ,  10.0, 20.0, 10.0)
+st.title('Streamlit for charts')
 
 df1 = px.data.gapminder().query("year == 2007")
 fig1 = px.treemap(df1, path=[px.Constant('World'), 'continent','country'], values = 'pop' , color = 'lifeExp')
 st.plotly_chart(fig1)
-
